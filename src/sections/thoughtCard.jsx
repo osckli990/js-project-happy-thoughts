303457df92ba3850285f53cc5eed2b0792e0cfc8
Oscar Liljefors <oscar.lf@live.se>
2025-05-09 16:02:06 +0200

    last changes

--- src/sections/thoughtCard.jsx
+++ src/sections/thoughtCard.jsx
@@ -2,19 +2,25 @@ import { Heart } from "../components/Heart";
 import { Thought } from "../components/Thought";
 import { Time } from "../components/Time";
 
+import { motion } from "framer-motion";
+/*using framer motion to easily add animation. not supported by eslint?*/
+
 export const ThoughtCard = ({ thought, setThoughts }) => {
   return (
     <>
       {thought.map((item) => (
-        <article
+        <motion.article
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4 }}
           key={item._id}
           id="ThoughtCard"
-          className="grid grid-cols-2 w-full border-1 border-bordergrey  shadow-smallscreenbox sm:shadow-box p-[20px] items-center box-border animate-[wiggle_1s_ease-in-out]"
+          className="grid grid-cols-2 w-full border-1 border-bordergrey  shadow-smallscreenbox sm:shadow-box p-[20px] items-center"
         >
           <Thought message={item.message} />
           <Heart id={item._id} hearts={item.hearts} setThoughts={setThoughts} />
           <Time time={item.createdAt} />
-        </article>
+        </motion.article>
       ))}
     </>
   );
